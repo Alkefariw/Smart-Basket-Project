c7419368ee1b7c607c58c7924f110f5d9820b7c2
Alkefariw <98129759+Alkefariw@users.noreply.github.com>
2023-06-26 15:31:45 -0400

    Update book.js

--- Smart Basket Project/js/book.js
+++ Smart Basket Project/js/book.js
@@ -244,7 +244,7 @@ function resetButtons(){
 
 function dataURLtoBlob(dataURI) {
     // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+   
     var byteString = atob(dataURI.split(',')[1]);
 
     // separate out the mime component
